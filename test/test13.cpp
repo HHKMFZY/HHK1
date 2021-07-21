a49baad6af329a46783557d8b03451a45579ef00
HHKMFZY <453674257@qq.com>
2021-07-21 10:21:38 +0800

    0721test

---
+++ test/test13.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{    
+    char string[10];
+    while(string!=0)
+    {
+    cout << "ÊäÈë: ";
+    cin >> string;
+ 
+    cout << "ÊäÈëÎª: " << string; 
+	}
+	   
+    return 0;
+}
